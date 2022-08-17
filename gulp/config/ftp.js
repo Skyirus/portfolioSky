4c8ec215b49861c75f3a0e19b20b8b09e3a8c0fb
Skyirus <91524181+Skyirus@users.noreply.github.com>
2022-08-17 19:13:57 +0200

    version 1

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+    host: "p414287.ftp.ihc.ru", // Адрес FTP сервера
+    user: "p414287_serv", // Имя пользователя
+    password: "cWsdmP5pwh", // Пароль
+    parallel: 5 // Кол-во одновременных потоков
+}
